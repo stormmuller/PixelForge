96ca9e5b7c282e84afbadf8e80a0af77685ad840
Storm <stormmuller2@gmail.com>
2025-02-06 16:42:01 +0200

    refactor: clean up texture creation code by removing unnecessary comments

--- src/rendering/shaders/utils/create-texture-from-image.ts
+++ src/rendering/shaders/utils/create-texture-from-image.ts
@@ -5,13 +5,11 @@ export const createTextureFromImage = (
   const texture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
 
-  // Set up texture so we can render any size image
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
-  // Upload the image into the texture
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 
   return texture;
