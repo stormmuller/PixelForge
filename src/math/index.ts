c1f436a86e8ed506e94472373ad5a0a9a2cefc1a
Storm <stormmuller2@gmail.com>
2025-02-08 06:03:52 +0200

    feat: implement Matrix3x3 class

--- src/math/index.ts
+++ src/math/index.ts
@@ -2,6 +2,7 @@ export * from './calculate-box-collider';
 export * from './clamp';
 export * from './degrees-to-radians';
 export * from './lerp';
+export * from './matrices';
 export * from './random';
 export * from './scale-relative-to-point';
 export * from './types';
