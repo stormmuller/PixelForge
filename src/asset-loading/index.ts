c9c43b1209afeb90f810921817762da126e6c908
Storm <stormmuller2@gmail.com>
2025-02-08 20:38:16 +0000

    refactor: remove asset loading components and related types

--- src/asset-loading/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './components';
-export * from './loading-group';
-export * from './types';
--- src/asset-loading/types/asset-cache.ts
+++
@@ -1,6 +0,0 @@
-export interface AssetCache<T> {
-  assets: Map<string, T>;
-  get: (path: string) => T;
-  load: (path: string) => Promise<void>;
-  getOrLoad: (path: string) => Promise<T>;
-}
