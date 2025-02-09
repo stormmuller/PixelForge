3814a53f0eaba0d9e001258038821656fdb88cf5
Storm <stormmuller2@gmail.com>
2025-02-09 19:16:53 +0000

    fix: add server host configuration for local development

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,9 @@ import path from 'path';
 
 export default defineConfig({
   root: path.join(__dirname, 'demo'),
+  server: {
+    host: '127.0.0.1', // see https://vite.dev/guide/troubleshooting.html#dev-containers-vs-code-port-forwarding
+  },
   build: {
     target: 'es2022',
   },
