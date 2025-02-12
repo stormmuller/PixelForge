175cbde3d59c362538853ce5751b8c8d1e28dde9
Storm <stormmuller2@gmail.com>
2025-02-12 20:46:42 +0000

    docs: enhance documentation for collider and box collider components

--- src/physics/colliders/collider.ts
+++ src/physics/colliders/collider.ts
@@ -1,12 +1,41 @@
 import { Vector2 } from '../../math';
 
+/**
+ * The `Collider` interface defines the structure for collider objects.
+ * It provides methods to check containment, combine with other colliders,
+ * and calculate bounding boxes.
+ */
 export interface Collider<T extends Collider<T>> {
+  /** The maximum x-coordinate of the collider */
   maxX: number;
+
+  /** The minimum x-coordinate of the collider */
   minX: number;
+
+  /** The maximum y-coordinate of the collider */
   maxY: number;
+
+  /** The minimum y-coordinate of the collider */
   minY: number;
 
+  /**
+   * Checks if a given point is contained within the collider.
+   * @param point - The point to check.
+   * @returns `true` if the point is within the collider, `false` otherwise.
+   */
   contains(point: Vector2): boolean;
+
+  /**
+   * Combines this collider with another collider to create a new bounding box.
+   * @param other - The other collider to combine with.
+   * @returns A new collider representing the combined bounding box.
+   */
   combine(other: T): T;
+
+  /**
+   * Combines this collider with an array of other colliders to create a new bounding box.
+   * @param others - An array of other colliders to combine with.
+   * @returns A new collider representing the combined bounding box.
+   */
   combineAll(others: T[]): T;
 }
--- src/physics/components/box-collider-component.ts
+++ src/physics/components/box-collider-component.ts
@@ -1,15 +1,26 @@
 import { Component } from '../../ecs';
 import { BoxCollider } from '../colliders';
 
+/**
+ * The `BoxColliderComponent` class implements the `Component` interface and represents
+ * a component that contains a `BoxCollider`.
+ */
 export class BoxColliderComponent implements Component {
+  /** The name property holds the unique symbol for this component. */
   public name: symbol;
+
+  /** The `BoxCollider` instance associated with this component. */
   public boxCollider: BoxCollider;
 
+  /** A static symbol property that uniquely identifies the `BoxColliderComponent`. */
   public static symbol = Symbol('BoxCollider');
 
+  /**
+   * Constructs a new instance of the `BoxColliderComponent` class with the given `BoxCollider`.
+   * @param boxCollider - The `BoxCollider` instance to associate with this component.
+   */
   constructor(boxCollider: BoxCollider) {
     this.name = BoxColliderComponent.symbol;
-
     this.boxCollider = boxCollider;
   }
 }
